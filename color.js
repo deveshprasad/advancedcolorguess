3ceae0944b8c62efdd04327a4ee7558548742e7d
deveshprasad <63739986+deveshprasad@users.noreply.github.com>
2020-06-30 17:45:13 +0530

    Update color.js

--- color.js
+++ color.js
@@ -25,7 +25,7 @@ function setupModeButtons(){
 	for(var i = 0; i < modeButtons.length; i++){
 		modeButtons[i].addEventListener("click", function(){
 			modeButtons[0].classList.remove("selected");
-			modeButtons[1].classList.remove("selected");
+			modeButtons[1].classList.reve("selected");
 			modeButtons[2].classList.remove("selected");
 			this.classList.add("selected");
 			if(this.textContent === "Easy"){
